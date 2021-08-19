1c21c54dc2212fa0a992f909eeb38a6ba43adad5
carlos <carlos.dan.pvst@gmail.com>
2021-08-19 17:01:21 -0500

    Ya se pueden agregar servicios a través del panel de administrador

--- js/serviciosPanel.js
+++ js/serviciosPanel.js
@@ -3,84 +3,84 @@ if( window.localStorage.getItem("serviciosLocalS") == null ){
 	let datosJson = [
 	{
 		"imagen": "./img/services/img-service1.jpg",
-		"nombre-Servicio": "Curso Básico de Protección Radiológica Nivel Personal Ocupacionalmente Expuesto (POE)",
-		"presentacion-Servicio": "Curso dirigido a técnicos radiólogos, médicos radiólogos, enfermeras, cirujanos dentistas, médicos de diferentes especialidades y cualquier personal ocupacionalmente expuesto (POE) que haga uso de algún equipo de rayos X para diagnóstico médico.",
+		"titulo": "Curso Básico de Protección Radiológica Nivel Personal Ocupacionalmente Expuesto (POE)",
+		"parrafo1": "Curso dirigido a técnicos radiólogos, médicos radiólogos, enfermeras, cirujanos dentistas, médicos de diferentes especialidades y cualquier personal ocupacionalmente expuesto (POE) que haga uso de algún equipo de rayos X para diagnóstico médico.",
 		"parrafo2": "Este curso está diseñado para que el alumno comprenda fácilmente y de forma didáctica qué son las radiaciones ionizantes y cuáles son los principios de protección radiológica que puede aplicar en su lugar de trabajo.",
 		"parrafo3": "El contenido del curso se basa en lo estipulado en las Normas Oficiales Mexicanas NOM-229-SSA1-2002 y NOM-031-NUCL-2011.",
-		"parrafoRes": "Duración del curso: 40 horas.",
+		"parrafoBlack": "Duración del curso: 40 horas.",
 		"id": "1"
 	},
 	{
 		"imagen": "./img/services/img-service2.jpg",
-		"nombre-Servicio": "Curso de Actualización en Protección Radiológica para Personal Ocupacionalmente Expuesto (POE)",
-		"presentacion-Servicio": "Curso dirigido al personal ocupacionalmente expuesto (POE) que labora en establecimientos de diagnóstico médico con rayos X y cuenta con un Curso Básico de Protección Radiológica Nivel POE. ",
+		"titulo": "Curso de Actualización en Protección Radiológica para Personal Ocupacionalmente Expuesto (POE)",
+		"parrafo1": "Curso dirigido al personal ocupacionalmente expuesto (POE) que labora en establecimientos de diagnóstico médico con rayos X y cuenta con un Curso Básico de Protección Radiológica Nivel POE. ",
 		"parrafo2": "RadioMed le ofrece un Curso de Actualización personalizado, de acuerdo al tipo de establecimiento médico que lo solicita, con el fin de reforzar en el alumno los conocimientos prácticos de protección radiológica que le serán útiles en sus actividades laborales diarias.",
 		"parrafo3": "El Curso de Actualización se imparte de forma anual de acuerdo a lo establecido en las Normas Oficiales Mexicanas NOM-229-SSA1-200 y NOM-031-NUCL-2011.",
-		"parrafoRes": "Duración del curso: 8 a 16 horas.",
+		"parrafoBlack": "Duración del curso: 8 a 16 horas.",
 		"id": "2"
 	},
 	{
 		"imagen": "./img/services/img-service3.jpg",
-		"nombre-Servicio": "La dosimetría personal más moderna y precisa del mercado a un precio accesible.",
-		"presentacion-Servicio": "Tecnología alemana BeOSL, ampliamente conocida y utilizada en países como Italia, Japón, Suiza, Bélgica, EUA y muchos más. ",
+		"titulo": "La dosimetría personal más moderna y precisa del mercado a un precio accesible.",
+		"parrafo1": "Tecnología alemana BeOSL, ampliamente conocida y utilizada en países como Italia, Japón, Suiza, Bélgica, EUA y muchos más. ",
 		"parrafo2": "Nuestro servicio está avalado por la Comisión Nacional de Seguridad Nuclear y Salvaguardias (CNSNS) mediante el Programa Permanente de Supervisión de Servicios de Dosimetría (PPSSD). ",
 		"parrafo3": " ",
-		"parrafoRes": "¡Pregunte por nuestros descuentos y proporcione dosimetría de calidad a todo su personal! ",
+		"parrafoBlack": "¡Pregunte por nuestros descuentos y proporcione dosimetría de calidad a todo su personal! ",
 		"id": "3"
 	},
 	{
 		"imagen": "./img/services/img-service4.jpg",
-		"nombre-Servicio": "Gestoría ante la COFEPRIS",
-		"presentacion-Servicio": "Lo apoyamos en la obtención de su Licencia Sanitaria y Permiso del Responsable de la Operación y el funcionamiento. Nuestro servicio está garantizado. ",
+		"titulo": "Gestoría ante la COFEPRIS",
+		"parrafo1": "Lo apoyamos en la obtención de su Licencia Sanitaria y Permiso del Responsable de la Operación y el funcionamiento. Nuestro servicio está garantizado. ",
 		"parrafo2": " ",
 		"parrafo3": " ",
-		"parrafoRes": "Le proporcionamos Manuales de Protección Radiológica y Garantía de Calidad personalizados, de acuerdo a las necesidades de su establecimiento, todo con base en lo requerido por la Norma Oficial Mexicana NOM-229-SSA1-2002.",
+		"parrafoBlack": "Le proporcionamos Manuales de Protección Radiológica y Garantía de Calidad personalizados, de acuerdo a las necesidades de su establecimiento, todo con base en lo requerido por la Norma Oficial Mexicana NOM-229-SSA1-2002.",
 		"id": "4"
 	},
 	{
 		"imagen": "./img/services/img-service5.jpg",
-		"nombre-Servicio": "Memoria Analítica",
-		"presentacion-Servicio": "Cálculo de blindajes para su sala de rayos X.",
+		"titulo": "Memoria Analítica",
+		"parrafo1": "Cálculo de blindajes para su sala de rayos X.",
 		"parrafo2": "Evite gastos innecesarios, le indicamos la forma más efectiva de blindar su sala cumpliendo con la norma mexicana y cuidando su presupuesto.",
 		"parrafo3": "Adecuamos completamente su sala de rayos X, pregunte por nuestro servicio de blindaje de salas, manejamos plomo y barita.",
-		"parrafoRes": "Le proporcionamos un certificado con validez oficial ante la COFEPRIS.",
+		"parrafoBlack": "Le proporcionamos un certificado con validez oficial ante la COFEPRIS.",
 		"id": "5"
 	},
 	{
 		"imagen": "./img/services/img-service6.jpg",
-		"nombre-Servicio": "Levantamiento de niveles",
-		"presentacion-Servicio": "Verificación de los blindajes de su sala de rayos X.",
+		"titulo": "Levantamiento de niveles",
+		"parrafo1": "Verificación de los blindajes de su sala de rayos X.",
 		"parrafo2": "Las puebas se realizan con detector de radiación tipo cámara de ionización como lo establece la Norma Oficial Mexicana NOM-229-SSA1-2002.",
 		"parrafo3": " ",
-		"parrafoRes": "Le proporcionamos un certificado con validez oficial ante la COFEPRIS.",
+		"parrafoBlack": "Le proporcionamos un certificado con validez oficial ante la COFEPRIS.",
 		"id": "6"
 	},
 	{
 		"imagen": "./img/services/img-service7.jpg",
-		"nombre-Servicio": "Control de Calidad para Equipos de Diagnóstico Médico con Rayos X ",
-		"presentacion-Servicio": "Somos Asesores Especializados en Seguridad Radiológica avalados por la COFEPRIS. Lo ayudamos a cumplir con el Programa de Garantía de Calidad solicitado por la Norma Oficial Mexicana NOM-229-SSA1-2002.",
+		"titulo": "Control de Calidad para Equipos de Diagnóstico Médico con Rayos X ",
+		"parrafo1": "Somos Asesores Especializados en Seguridad Radiológica avalados por la COFEPRIS. Lo ayudamos a cumplir con el Programa de Garantía de Calidad solicitado por la Norma Oficial Mexicana NOM-229-SSA1-2002.",
 		"parrafo2": "Realizamos pruebas de control de calidad a sus equipos de rayos X convencionales, tomógrafos, mastógrafos, rayos X dentales, fluoroscopía, etc.",
 		"parrafo3": "Utilizamos equipos y sensores de última generación para asegurar la protección del POE y de sus pacientes.  ",
-		"parrafoRes": "Nuestros certificados de pruebas de control de calidad tienen validez oficial ante la COFEPRIS.",
+		"parrafoBlack": "Nuestros certificados de pruebas de control de calidad tienen validez oficial ante la COFEPRIS.",
 		"id": "7"
 	},
 	{
 		"imagen": "./img/services/img-service8.jpg",
-		"nombre-Servicio": "Auditoría interna en protección radiológica",
-		"presentacion-Servicio": "Visitamos su establecimiento para evaluar las condiciones generales de protección radiológica en su sala de rayos X, con base en los requerimientos de la Norma Oficial Mexicana NOM-229-SSA1-2002.",
+		"titulo": "Auditoría interna en protección radiológica",
+		"parrafo1": "Visitamos su establecimiento para evaluar las condiciones generales de protección radiológica en su sala de rayos X, con base en los requerimientos de la Norma Oficial Mexicana NOM-229-SSA1-2002.",
 		"parrafo2": "Se realiza una revisión a su expediente de seguridad radiológica y se entrega un dictamen con la evaluación de su sala, indicando los puntos que deben atenderse para cumplir con lo establecido por la COFEPRIS.",
 		"parrafo3": " ",
-		"parrafoRes": "Incluye una plática orientativa sobre el Programa de Garantía de Calidad establecido en la Norma Oficial Mexicana NOM-229-SSA1-2002.",
+		"parrafoBlack": "Incluye una plática orientativa sobre el Programa de Garantía de Calidad establecido en la Norma Oficial Mexicana NOM-229-SSA1-2002.",
 		"id": "8"
 	},
 
 	{
 		"imagen": "./img/services/img-service9.jpg",
-		"nombre-Servicio": "Señalamientos de protección radiológica",
-		"presentacion-Servicio": "Le proporcionamos los señalamientos de protección radiológica necesarios para su establecimiento de diagnóstico médico.",
+		"titulo": "Señalamientos de protección radiológica",
+		"parrafo1": "Le proporcionamos los señalamientos de protección radiológica necesarios para su establecimiento de diagnóstico médico.",
 		"parrafo2": " ",
 		"parrafo3": " ",
-		"parrafoRes": "Nuestros señalamientos cumplen con los estándares exigidos por la Norma Oficial Mexicana NOM-229-SSA1-2002.",
+		"parrafoBlack": "Nuestros señalamientos cumplen con los estándares exigidos por la Norma Oficial Mexicana NOM-229-SSA1-2002.",
 		"id": "9"
 	},
 	]
