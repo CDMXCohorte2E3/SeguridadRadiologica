9c6813d2683abbdef17bf11be3a7374ee36a5777
America <americasanchezleon@gmail.com>
2021-08-23 10:18:39 -0500

    FEAT | añadí los archivos que faltaban de la base de datos

--- js/carrito.js
+++ js/carrito.js
@@ -13,10 +13,11 @@ function comprarCart(){
 }// comprarCart
 
 
+//Funcion para eliminar row
 function DeleteRowFunction() {
     // event.target will be the input element.
     var td = event.target.parentNode; 
     var tr = td.parentNode; // the row to be removed
     tr.parentNode.removeChild(tr);
-}
+}//DeleteRowFunction
 
--- js/productos.js
+++ js/productos.js
@@ -140,12 +140,11 @@ function anadirProducto(productos){
                     </ul> 
                 </p> 
                 <div class="button text-center">      
-                  <button class="btn btn-info btn-add-car" onclick="anadirCarrito();">Comprar</button>   
+                  <button class="btn btn-info btn-add-car" id="btn-add-car_${producto.id}" onclick="anadirCarrito(${producto.id});">Comprar</button>   
                 </div>   
             </div> <!--card-body--> 
         </div><!--card--> 
         </div>`
-
         plantilla += card;
     }) // Fin del forEach
     ancla.innerHTML = plantilla;
@@ -187,9 +186,38 @@ listaElementos(almacenLocal);
 
 
 //funcion para añadir producto al carrito de compras
-function anadirCarrito(productos){
-  
+function anadirCarrito(producto){
+
   var anclaCart = document.getElementById('textoPrueba');
-  anclaCart.innerHTML += `${productos.titulo}`;
+  var idProd = document.getElementById('btn-add-car_${producto.id}');
+
+
+
+  anclaCart.innerHTML += ` <tr>
+  <th scope="row" id="productoCarrito col-6">Titulo</th>
+  <td class="col-3"><input type="number" value="1"></td>
+  <td style="text-align: center;" class="col-3">
+
+      <button type="button" class="btn btn-outline-danger" value="Delete Row"
+          onclick="DeleteRowFunction()">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+              class="bi bi-trash" viewBox="0 0 16 16">
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 
+                  .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 
+                  .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z">
+              </path>
+              <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1
+                   0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 
+                   1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z">
+              </path>
+          </svg>
+          Eliminar
+      </button>
+  </td>
+</tr> <br> `;
+    
+  console.log("Si funciona");
 
 }//anadir carrito
+
+
