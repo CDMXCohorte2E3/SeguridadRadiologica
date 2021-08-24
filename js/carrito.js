24cce2c7fa61df6d1706dcf7d630717d3dc2e411
CarlosDanPVST <86733237+CarlosDanPVST@users.noreply.github.com>
2021-08-23 22:05:05 -0500

    Borré código comentado

--- js/carrito.js
+++ js/carrito.js
@@ -97,27 +97,3 @@ if( window.localStorage.getItem("identificadoresLocalS") != null){
     }
     cargarCompras();
 }
-
-/*
-function removerUnRepetido(arr, value) {
-  var index = arr.indexOf(value);
-  if (index > -1) {
-    arr.splice(index, 1);
-  }
-  return arr;
-}
-function removeTodoRepetidos(arr, value) {
-  var i = 0;
-  while (i < arr.length) {
-    if (arr[i] === value) {
-      arr.splice(i, 1);
-    } else {
-      ++i;
-    }
-  }
-  return arr;
-}
-// Usage
-console.log(removeItemOnce([2,5,9,1,5,8,5], 5))
-console.log(removeItemAll([2,5,9,1,5,8,5], 5))
-*/
